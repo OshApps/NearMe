03571c88cd9f03893ff059096703a818220be9e8
oshri6688 <oshria6688@gmail.com>
2016-06-17 11:51:30 +0300

    Add fragment for wrap the fragments are shown in tabs

---
+++ app/src/main/java/com/osh/apps/maps/fragment/TabFragment.java
@@ -0,0 +1,20 @@
+package com.osh.apps.maps.fragment;
+
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Created by oshri-n on 15/06/2016.
+ */
+public abstract class TabFragment extends Fragment
+{
+
+   public TabFragment()
+   {
+
+   }
+
+
+   abstract public int getTitleRes();
+
+}
