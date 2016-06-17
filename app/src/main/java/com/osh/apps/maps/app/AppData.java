4d8f84b7d27fd6c37741f390295d276dd8ea2476
oshri6688 <oshria6688@gmail.com>
2016-06-17 12:24:41 +0300

    Update AppData for contain the places api constants

--- app/src/main/java/com/osh/apps/maps/app/AppData.java
+++ app/src/main/java/com/osh/apps/maps/app/AppData.java
@@ -7,60 +7,97 @@ public final class AppData
 {
 public static final int NULL_DATA=-1;
 
-	private AppData(){}
 
-
-    public static final class IntentKey
-	{
-		public static final String IMDB_ID_KEY="imdbID";
-        public static final String MOVIE_ID_KEY ="movieID" ;
+    public static final class Place
+    {
+    public static final int SEARCH_TYPE=0;
+    public static final int FAVOURITE_TYPE=1;
     }
 
 
-	public static final class OMDB
-	{
-		public static final String OMDB_REQUEST_URL="http://www.omdbapi.com/?";
-		public static final String OMDB_FILTER_MOVIE_PARAMETER="&type=movie";
-		public static final String OMDB_PAGE_PARAMETER="&page=";
-		public static final String OMDB_SEARCH_PARAMETER="s=";
-		public static final String OMDB_ID_PARAMETER="i=";
-
-		public static String getSearchFormat(String value)
-		{
-		return "*"+value.trim().replaceAll("\\s","%20")+"*";
-		}
-	}
-
-
-	public static final class Database
+	public static final class PlacesAPI
     {
-        public static final String DATABASE_NAME="Osh-Movies.db";
-		public static final int DATABASE_VERSION=1;
+    private static final String BASE_URL="https://maps.googleapis.com/maps/api/place/";
 
-        public static final class Movies
+    private static final int PHOTO_MAX_SIZE=1600;
+
+    private static final String SEARCH_REQUEST="nearbysearch/json?";
+    private static final String DETAILS_REQUEST="details/json?";
+    private static final String PHOTO_REQUEST="photo?";
+
+    private static final String PLACE_ID_FIRST_PARAMETER="placeid=";
+    private static final String LOCATION_FIRST_PARAMETER="location=";
+    private static final String MAX_WIDTH_FIRST_PARAMETER="maxwidth=";
+    private static final String MAX_HEIGHT_FIRST_PARAMETER="maxheight=";
+
+    private static final String RADIUS_PARAMETER="&radius=";
+    private static final String KEYWORD_PARAMETER="&keyword=";
+    private static final String KEY_PARAMETER="&key=";
+    private static final String PHOTO_REFERENCE_PARAMETER="&photoreference=";
+
+
+        private static String getKeywordFormat(String keyword)
         {
-            public static final String TABLE_NAME="movies";
-
-            private static final String [] NAME_COL={"movieID","imdbID","title","genre","language","plot","year","poster","rating"};
-
-            public static final int COL_MOVIE_ID=0;
-            public static final int COL_IMDB_ID=1;
-            public static final int COL_TITLE=2;
-            public static final int COL_GENRE=3;
-            public static final int COL_LANGUAGE=4;
-            public static final int COL_PLOT=5;
-            public static final int COL_YEAR=6;
-            public static final int COL_POSTER=7;
-            public static final int COL_RATING=8;
+        return keyword.trim().replaceAll("\\s","%20");
+        }
 
 
+        public static String createSearchUrl(String keyword, double lat, double lng, int radius, String apiKey)
+        {
+        String url=BASE_URL;
 
-            public static String getColName(int col)
+        url+= SEARCH_REQUEST;
+        url+= LOCATION_FIRST_PARAMETER + lat + "," + lng;
+        url+= RADIUS_PARAMETER + radius;
+
+        if(keyword != null)
             {
-            return NAME_COL[col];
+            url+=KEYWORD_PARAMETER + getKeywordFormat(keyword);
             }
 
+        url+=KEY_PARAMETER + apiKey;
+
+        return url;
         }
+
+
+        public static String createDetailsUrl(String googleId, String apiKey)
+        {
+        String url=BASE_URL;
+
+        url+= DETAILS_REQUEST;
+        url+= PLACE_ID_FIRST_PARAMETER + googleId;
+        url+= KEY_PARAMETER + apiKey;
+
+        return url;
+        }
+
+
+        public static String createPhotoUrl(String photoReference, String apiKey)
+        {
+        return createPhotoUrl(photoReference, PHOTO_MAX_SIZE, PHOTO_MAX_SIZE, apiKey);
+        }
+
+
+        public static String createPhotoUrl(String photoReference, int width, int height, String apiKey)
+        {
+        String url=BASE_URL;
+
+        url+= PHOTO_REQUEST;
+
+        width= (width==NULL_DATA)? PHOTO_MAX_SIZE : width;
+        height= (height==NULL_DATA)? PHOTO_MAX_SIZE : height;
+
+        url+= MAX_WIDTH_FIRST_PARAMETER + width + "&" +MAX_HEIGHT_FIRST_PARAMETER + height;
+
+
+        url+= PHOTO_REFERENCE_PARAMETER + photoReference;
+        url+= KEY_PARAMETER + apiKey;
+
+        return url;
+        }
+
     }
+
 
 }
