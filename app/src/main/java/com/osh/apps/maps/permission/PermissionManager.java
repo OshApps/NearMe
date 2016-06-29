02747feb90be5554e23671546d196039d3ec2a30
oshri6688 <oshria6688@gmail.com>
2016-06-29 15:23:21 +0300

    Updated PermissionManager for handling request from activity

--- app/src/main/java/com/osh/apps/maps/permission/PermissionManager.java
+++ app/src/main/java/com/osh/apps/maps/permission/PermissionManager.java
@@ -1,7 +1,10 @@
 package com.osh.apps.maps.permission;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
@@ -9,19 +12,37 @@ import android.support.v4.content.ContextCompat;
 public class PermissionManager
 {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public static boolean checkPermission(Fragment fragment, String permission, int requestCode )
     {
-    boolean hasPermission=true;
+    boolean hasPermission=hasPermission(fragment.getActivity(), permission);
 
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+    if(!hasPermission)
         {
-        if(ContextCompat.checkSelfPermission(fragment.getActivity(), permission) != PackageManager.PERMISSION_GRANTED)
-            {
-            fragment.requestPermissions(new String[]{permission}, requestCode);
-            hasPermission=false;
-            }
+        fragment.requestPermissions(new String[]{permission}, requestCode);
         }
 
     return hasPermission;
     }
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static boolean checkPermission(Activity activity, String permission, int requestCode )
+    {
+    boolean hasPermission=hasPermission(activity, permission);
+
+    if(!hasPermission)
+        {
+        ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+        }
+
+    return hasPermission;
+    }
+
+
+    private static boolean hasPermission(Activity activity, String permission )
+    {
+    return (Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) || ( ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED);
+    }
+
 }
